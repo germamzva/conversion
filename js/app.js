f99ae814c0f8371d1b0c79cc0e2377cf434ba126
germamzva <ranfeche@gmail.com>
2021-03-23 16:31:07 +0800

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,14 +1,32 @@
 $(document).ready(function(){
 
 	// tab content
-var triggerTabList = [].slice.call(document.querySelectorAll('#tab_block a'))
-triggerTabList.forEach(function (triggerEl) {
-  var tabTrigger = new bootstrap.Tab(triggerEl)
+  var triggerTabList = [].slice.call(document.querySelectorAll('#tab_block a'))
+  triggerTabList.forEach(function (triggerEl) {
+    var tabTrigger = new bootstrap.Tab(triggerEl)
 
-  triggerEl.addEventListener('click', function (event) {
-    event.preventDefault()
-    tabTrigger.show()
-  })
-})
+    triggerEl.addEventListener('click', function (event) {
+      event.preventDefault()
+      tabTrigger.show()
+    })
+  });
+
+  // tv page product carousel
+  $("#productSlider").owlCarousel({
+    items: 3,
+    margin: 15,
+    trl: true,
+    lazyLoad: true,
+    autoHeight: true,
+    autoWidth:true,
+    dots: false,
+    nav: true,
+    responsive : {
+      768: {
+        items: 1,
+        nav: false,
+      }
+    }
+  });
 
 });
